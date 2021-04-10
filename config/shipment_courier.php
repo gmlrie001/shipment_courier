379e8acb86bcace8925a2f50fbd727e08fcc3b58
gmlrie001 <dev2@monzamedia.com>
2021-04-10 14:09:01 +0200

    Services:added -> ShipmentDestination - completed.

--- config/shipment_courier.php
+++ config/shipment_courier.php
@@ -20,9 +20,47 @@
       
     ],
 
-    'shipmentOrderDestination' => [
+    'shipmentDestination' => [
+
+      'destinationStreetAddress' => 'delivery_address_line_1',
+      'destinationBusinessPark'  => 'delivery_address_line_2',
+      'destinationOtherAddress'  => 'delivery_address_line_2',
+      'destinationStateProvince' => 'delivery_province',
+      'destinationCountryCode'   => 'delivery_country',
+      'destinationCountryName'   => 'delivery_country',
+      'destinationSuburb'        => 'delivery_suburb',
+      'destinationPostalCode'    => 'delivery_postal_code',
+
+      'destinationContactName'   => 'delivery_company',
+
+      'destinationContactPerson' => 'delivery_name',
+      'destinationContactPhone'  => 'delivery_phone',
+      'destinationContactEmail'  => 'user.email',
+
+      'destinationReferenceArr'  => 'id', // 
 
     ],
+
+    'orderDeliveryKeys' => [
+
+      'delivery_address_line_1',
+      'delivery_address_line_2', 
+
+      'delivery_province',
+      'delivery_country', // 2 letter ISO-Code
+      'delivery_country', // Full name
+      'delivery_suburb',
+      'delivery_city', 
+      'delivery_postal_code',
+
+      'delivery_company',
+      'delivery_name',
+      'delivery_surname',
+      'delivery_phone',
+
+    ],
+
+    // ],
 
     'shipmentOrderItems' => [
 
