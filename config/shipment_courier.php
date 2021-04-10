0061da61e85627940ebce69b8a7fe4bdfb77ed09
M. Riedaa Gamieldien <61379972+gmlrie001@users.noreply.github.com>
2021-04-10 17:01:14 +0200

    Develop feature shipment origin addition (#4)
    * Updated config file - shipmentOrigin.
    
    * Services:added -> ShipmentOrigin - completed (see it as an example template.

--- config/shipment_courier.php
+++ config/shipment_courier.php
@@ -17,7 +17,22 @@
     ],
 
     'shipmentOrigin' => [
-      
+
+      'originStreetAddress' => "25 Upper Cambridge Street",
+      'originBusinessPark'  => NULL,
+      'originOtherAddress'  => "None",
+      'originStateProvince' => "Western Cape",
+      'originCountryCode'   => "ZA",
+      'originCountryName'   => "South Africa",
+      'originSuburb'        => "Walmer Estate",
+      'originPostalCode'    => "7925",
+
+      'originContactName'   => "Riedaa",
+
+      'originContactPerson' => "M. Riedaa Gamieldien",
+      'originContactPhone'  => "89412116",
+      'originContactEmail'  => "riedaa@monzamedia.com",
+
     ],
 
     'shipmentOrderDestination' => [
--- src/Services/ShipmentOrigin.php
+++ src/Services/ShipmentOrigin.php
@@ -13,70 +13,44 @@ use Illuminate\Support\Facades\Config;
 class ShipmentOrigin extends ShipmentCourier
 {
 
-  public $accountInfo;
+  public $shipmentOriginSettings;
 
 
   public function __construct()
   {
     parent::__construct();
 
-    $this->accountInfo = $this->shipmentConfig['accountInfo'];
+    $this->shipmentOriginSettings = $this->shipmentConfig['shipmentOrigin'];
+    $this->originKeys = array_keys( $this->shipmentOriginSettings );
 
     return $this;
   }
 
   public function setupOrigin()
   {
-    $this->setEmail($this->shipmentConfig['accountInfo']['email']);
-        //  ->getEmail();
-    $this->setPassword($this->shipmentConfig['accountInfo']['password']);
-        //  ->getPassword();
-    $this->setAccountNumber($this->shipmentConfig['accountInfo']['accountNumber']);
-        //  ->getAccountNumber();
-    $this->setAccountPin($this->shipmentConfig['accountInfo']['accountPin']);
-        //  ->getAccountPin();
+    foreach( $this->originKeys as $setting => $key ) {
+      dump( "KEY:=> " . $key . " _ " . "VALUE:=> " . $this->shipmentOriginSettings[$key] );
+
+      $value = $this->shipmentOriginSettings[$key];
+      $this->setProperty( $key, $value );
+
+      unset( $settings, $key, $value );
+    }
+
+    // $this->setEmail($this->shipmentConfig['accountInfo']['email']);
+    //     //  ->getEmail();
+    // $this->setPassword($this->shipmentConfig['accountInfo']['password']);
+    //     //  ->getPassword();
+    // $this->setAccountNumber($this->shipmentConfig['accountInfo']['accountNumber']);
+    //     //  ->getAccountNumber();
+    // $this->setAccountPin($this->shipmentConfig['accountInfo']['accountPin']);
+    //     //  ->getAccountPin();
+
+    $testKey   = 'originStreetAddress';
+    $testValue = $this->getProperty( $testKey );
+    dump( "\r\n", "TEST_KEY:=> " . $testKey . " _ " . "TEST_VALUE:=> " . $testValue, "\r\n" );
 
     return $this;
-  }
-
-  public function getEmail()
-  {
-    return $this->getProperty( 'clientEmail' );
-  }
-
-  public function setEmail( $value )
-  {
-    return $this->setProperty( 'clientEmail', $value );
-  }
-
-  public function getPassword( $key = 'clientPassword' )
-  {
-    return $this->getProperty( $key );
-  }
-  
-  public function setPassword( $value )
-  {
-    return $this->setProperty( 'clientPassword', $value );
-  }
-
-  public function getAccountNumber( $key = 'clientAccountNumber' )
-  {
-    return $this->getProperty( $key );
-  }
-  
-  public function setAccountNumber( $value )
-  {
-    return $this->setProperty( 'clientAccountNumber', $value );
-  }
-
-  public function getAccountPin( $key = 'clientAccountPin' )
-  {
-    return $this->getProperty( $key );
-  }
-  
-  public function setAccountPin( $value )
-  {
-    return $this->setProperty( 'clientAccountPin', $value );
   }
 
 }
