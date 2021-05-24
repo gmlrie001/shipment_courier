34b0428d2f9f53e51a50ee4e8f876180de40b5e0
gmlrie001 <dev2@monzamedia.com>
2021-05-24 16:39:33 +0200

    Username and password updated.

--- config/shipment_courier.php
+++ config/shipment_courier.php
@@ -72,16 +72,13 @@
     // Client specific account information/credentials
     // 'shipmentAccountInfo'
     'accountInfo' => [
-
-      'email'         => "riedaa@monzamedia.com",
-      'password'      => "12345678",
-      'accountNumber' => "094551",
-      'accountPin'    => "3254"
-    
+      'email'         => "monza@ecom",
+      'password'      => "monzaEcom",
+      'accountNumber' => "",
+      'accountPin'    => ""
     ],
 
     'shipmentOrigin' => [
-
       'originStreetAddress' => "25 Upper Cambridge Street",
       'originBusinessPark'  => NULL,
       'originOtherAddress'  => "None",
@@ -90,19 +87,14 @@
       'originCountryName'   => "South Africa",
       'originSuburb'        => "Walmer Estate",
       'originPostalCode'    => "7925",
-
       'originContactName'   => "Riedaa",
-
       'originContactPerson' => "M. Riedaa Gamieldien",
       'originContactPhone'  => "89412116",
       'originContactEmail'  => "riedaa@monzamedia.com",
-
-
       'originReference'     => NULL,
     ],
 
     'shipmentDestination' => [
-
       'destinationStreetAddress' => 'delivery_address_line_1',
       'destinationBusinessPark'  => 'delivery_address_line_2',
       'destinationOtherAddress'  => 'delivery_address_line_2',
@@ -111,51 +103,40 @@
       'destinationCountryName'   => 'delivery_country',
       'destinationSuburb'        => 'delivery_suburb',
       'destinationPostalCode'    => 'delivery_postal_code',
-
       'destinationContactName'   => 'delivery_company',
-
       'destinationContactPerson' => 'delivery_name',
       'destinationContactPhone'  => 'delivery_phone',
       'destinationContactEmail'  => 'user.email',
-
       'destinationReferenceArr'  => 'id', // 
-
     ],
 
     'orderDeliveryKeys' => [
-
       'delivery_address_line_1',
       'delivery_address_line_2', 
-
       'delivery_province',
       'delivery_country', // 2 letter ISO-Code
       'delivery_country', // Full name
       'delivery_suburb',
       'delivery_city', 
       'delivery_postal_code',
-
       'delivery_company',
       'delivery_name',
       'delivery_surname',
       'delivery_phone',
-
     ],
     // ],
 
     'shipmentOrderItems' => [
-
     ],
 
     'shipmentPickupDetails' => [
-
       'pickupComments'     => NULL,
       'pickupReference1'   => NULL,
       'pickupReference2'   => NULL,
       'pickupDate'         => NULL,
-      'pickupReadtTime'    => NULL,
+      'pickupReadyTime'    => NULL,
       'pickupClosingTime'  => NULL,
       'pickupEntityStatus' => NULL
-
     ],
 
   ];
